c59c6da2a4892d6baa57944fb8d91827f0b2c44a
mcicishvilii <mcicishvilii@gmail.com>
2022-12-22 18:30:19 +0400

    datepicker implementation

--- app/src/main/java/com/example/warehousemanagement/ui/screens/add/AddProductFragment.kt
+++ app/src/main/java/com/example/warehousemanagement/ui/screens/add/AddProductFragment.kt
@@ -1,10 +1,14 @@
 package com.example.warehousemanagement.ui.screens.add
 
 import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.example.warehousemanagement.R
 import com.example.warehousemanagement.common.BaseFragment
+import com.example.warehousemanagement.common.DatePickerFragment
 import com.example.warehousemanagement.data.model.Items
 import com.example.warehousemanagement.databinding.FragmentAddProductBinding
 import com.google.firebase.firestore.ktx.firestore
@@ -26,15 +30,27 @@ class AddProductFragment :
     override fun viewCreated() {
 
 
+
         binding.etBrandImpl.setText("Pianca")
         binding.etBoxQuantityImpl.setText("12")
-        binding.etSkuImpl.setText("12")
+//        binding.etSkuImpl.setText("12")
         binding.etProductNameImpl.setText("Skami")
 //        binding.etClientName.setText("misho")
+
+        setupCoworkerSpinner()
     }
 
     override fun listeners() {
         testAdd()
+        showDatePickerDialog()
+    }
+
+
+    fun showDatePickerDialog() {
+        binding.btnDate.setOnClickListener {
+            val newFragment = DatePickerFragment()
+            newFragment.show(parentFragmentManager, "datePicker")
+        }
     }
 
 
@@ -43,7 +59,7 @@ class AddProductFragment :
 
             val brand = binding.etBrandImpl.text.toString()
             val boxQuantity = binding.etBoxQuantityImpl.text.toString().toInt()
-            val sku = binding.etSkuImpl.text.toString()
+            val sku = "misho"
             val productName = binding.etProductNameImpl.text.toString()
 
             val abbreviationBrand = binding.etBrandImpl.text.toString()
@@ -70,4 +86,12 @@ class AddProductFragment :
 
         }
     }
+
+
+    private fun setupCoworkerSpinner() {
+        val coworker = resources.getStringArray(R.array.coworker)
+        val adapter = ArrayAdapter(requireContext(), R.layout.custom_spinner_layout,coworker)
+        binding.etCoworkerSpinner.setAdapter(adapter)
+    }
+
 }
